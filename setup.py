b3e3e84ab846126527c79dd084927ce2af0a7419
vekair <vekair@126.com>
2022-07-19 01:04:07 +0800

    update: 0.1.0

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kkdbs",
+    version="0.1.0",
+    description="数据库连接池",
+    long_description="数据库连接池",
+
+    url="https://github.com/vekair/kkdbs.git",
+    author="vekair",
+    author_email="vekair@126.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["pymysql", "PooledDB==1.3", "logging"]
+)
+
+"""
+打包上传
+python3 setup.py sdist
+
+twine upload dist/*
+
+python -m pip install kkexcel --upgrade -i https://pypi.org/simple   # 及时的方式，不用等待 阿里云 豆瓣 同步
+
+
+"""
